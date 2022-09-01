b3c41068cdca64246152c2b8ba50d83127df4e1e
sravani <sravani97pappula@gmail.com>
2022-09-01 16:33:44 +0530

    HIS-300:Related changes

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+		private static final string REST_URL="https://WWW.equifax.com/getScores";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
